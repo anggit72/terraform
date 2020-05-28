a33f880d74d60b898028a8209de3200ccc5e6827
anggit72 <anggit.indera@icloud.com>
2020-05-28 23:01:29 +0700

    Update webapp-sg.tf

--- site/webapp-sg.tf
+++ site/webapp-sg.tf
@@ -6,13 +6,7 @@ resource "aws_security_group" "webapp_http_inbound_sg" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }  
+  } 
   egress {
     from_port   = 0
     to_port     = 0
