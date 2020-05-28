022a15a89da52f4036fc46ab0331d0fc4b2e13b0
anggit72 <anggit.indera@icloud.com>
2020-05-29 00:49:30 +0700

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,5 +2,8 @@ terraform {
   backend "s3" {
     bucket = "terraform.state123"
     region = "ap-southeast-1"
+    key = "terraform.tfstate" 
+    access_key = "AKIAXCBJZCNLRPOBT5P7"
+    secret_key = "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"
   }
 }
